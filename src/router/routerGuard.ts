d7f27058a5454a725e0ffd8601d51342c6990ecc
liguanjie <1249850908@qq.com>
2023-09-26 15:24:44 +0800

    修改

--- src/router/routerGuard.ts
+++ src/router/routerGuard.ts
@@ -9,7 +9,7 @@ import type { Router } from 'vue-router';
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 const whiteList = ['login', 'register', 'registerResult'] // 不进行拦截的路由名称集合
-const defaultRoutePath = '/dashboard/analysis'
+const defaultRoutePath = '/Tianyi-miku.github.io-vuedemo/dashboard/analysis'
 
 export const setupBeforeEach = (router: Router) => {
   router.beforeEach((to, from, next) => {
